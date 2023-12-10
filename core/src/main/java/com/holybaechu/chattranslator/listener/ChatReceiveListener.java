5a7af37d7d4fd6d5f1a67b6ee397f43520cb1002
RappyTV <rappytv@rappytv.com>
2023-12-10 05:19:29 +0100

    Add constants for patterns, create local variable for config

--- core/src/main/java/com/holybaechu/chattranslator/listener/ChatReceiveListener.java
+++ core/src/main/java/com/holybaechu/chattranslator/listener/ChatReceiveListener.java
@@ -1,6 +1,7 @@
 package com.holybaechu.chattranslator.listener;
 
 import com.holybaechu.chattranslator.ChatTranslatorAddon;
+import com.holybaechu.chattranslator.ChatTranslatorConfiguration;
 import com.holybaechu.chattranslator.translators.BaseTranslator;
 import com.holybaechu.chattranslator.translators.GoogleTranslator;
 import net.labymod.api.client.chat.ChatMessage;
@@ -13,75 +14,66 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class ChatReceiveListener {
-  private final ChatTranslatorAddon addon;
+  private final ChatTranslatorConfiguration config;
   private BaseTranslator translator;
   private String targetLang;
 
+  private final Pattern mcDefaultRegex = Pattern.compile("^(\\<.*\\> )(.*)");
+  private final Pattern hypixelDefaultRegex = Pattern.compile("^(.*\\: )(.*)");
+
   public ChatReceiveListener(ChatTranslatorAddon addon) {
-    this.addon = addon;
+    this.config = addon.configuration();
   }
 
-  private String getGroupFromRegex(Pattern regex, String input, int n){
-    if (input.matches(regex.pattern())){
+  private String getGroupFromRegex(Pattern regex, String input, int n) {
+    if (input.matches(regex.pattern())) {
       Matcher m = regex.matcher(input);
-      m.find();
       return m.group(n);
     }
     return null;
   }
 
   @Subscribe
-  public void onChatReceive(ChatReceiveEvent event) throws Exception {
-    switch (addon.configuration().translationPlatform().get()){
-      case google:
-        this.translator = new GoogleTranslator();
-        break;
+  public void onChatReceive(ChatReceiveEvent event) {
+    switch (config.translationPlatform().get()) {
       default:
         this.translator = new GoogleTranslator();
         break;
     }
-    this.targetLang = addon.configuration().targetLanguage().get().name().replace("_", "-");
+    this.targetLang = config.targetLanguage().get().name().replace("_", "-");
 
-    if (!addon.configuration().enabled().get()) return;
+    if (!config.enabled().get()) return;
 
     ChatMessage message = event.chatMessage();
     GameProfile sender = message.getSenderProfile();
     String messageStr = message.getFormattedText();
 
-    Pattern mcDefaultRegex = Pattern.compile("^(\\<.*\\> )(.*)");
-    Pattern hypixelDefaultRegex = Pattern.compile("^(.*\\: )(.*)");
-
     String finalMessageStr = messageStr;
-    if(sender != null){
+    if(sender != null) {
       if (getGroupFromRegex(mcDefaultRegex, finalMessageStr, 2) != null){
         finalMessageStr = getGroupFromRegex(mcDefaultRegex, finalMessageStr, 2);
-      }else if (getGroupFromRegex(hypixelDefaultRegex, finalMessageStr, 2) != null){
+      } else if (getGroupFromRegex(hypixelDefaultRegex, finalMessageStr, 2) != null) {
         finalMessageStr = getGroupFromRegex(hypixelDefaultRegex, finalMessageStr, 2);
       }
-    }else if(addon.configuration().doNotTranslateNonPlayerMessages().get()){
+    } else if (config.doNotTranslateNonPlayerMessages().get()) {
       return;
     }
 
     String finalMessageStr1 = finalMessageStr;
     Task.builder(() -> {
       try {
-        String translation = translator.translate("auto", targetLang,
-            finalMessageStr1);
+        String finalTranslation = translator.translate("auto", targetLang, finalMessageStr1);
 
-        String finalTranslation = translation;
-
-        if(sender != null){
+        if (sender != null){
           if (getGroupFromRegex(mcDefaultRegex, messageStr, 1) != null){
             finalTranslation = getGroupFromRegex(mcDefaultRegex, messageStr, 1) + finalTranslation;
-          }else if (getGroupFromRegex(hypixelDefaultRegex, messageStr, 1) != null){
+          } else if (getGroupFromRegex(hypixelDefaultRegex, messageStr, 1) != null){
             finalTranslation = getGroupFromRegex(hypixelDefaultRegex, messageStr, 1) + finalTranslation;
           }
         }
 
         String finalTranslation1 = finalTranslation;
-        Task.builder(() -> {
-          message.edit(Component.text(finalTranslation1));
-        }).build().executeOnRenderThread();
+        Task.builder(() -> message.edit(Component.text(finalTranslation1))).build().executeOnRenderThread();
       } catch (Exception e) {
         throw new RuntimeException(e);
       }
