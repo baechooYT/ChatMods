14962b2bae63a5cb0e178c86396b3904401905ef
holybaechu <joonhoo0524@naver.com>
2024-01-06 17:14:50 +0900

    Fix message not translating for some servers

--- core/src/main/java/com/holybaechu/chattranslator/listener/ChatReceiveListener.java
+++ core/src/main/java/com/holybaechu/chattranslator/listener/ChatReceiveListener.java
@@ -24,6 +24,33 @@ public class ChatReceiveListener {
     targetLanguage = newLanguage;
   }
 
+  private static void translateMessage(List<Component> components, ChatMessage message){
+    for (Component child : components) {
+      List<Component> childChildren = child.getChildren();
+      if (!childChildren.isEmpty()){
+        translateMessage(childChildren, message);
+      }
+
+      if (child instanceof TextComponent textComponent) {
+        Logging.getLogger().info(textComponent.getText());
+
+        if (Objects.equals(textComponent.getText(), ""))
+          continue;
+        if (Objects.equals(textComponent.getText(), " "))
+          continue;
+
+        try {
+          textComponent.text(translator.translate("auto", String.valueOf(targetLanguage),
+              textComponent.getText()));
+
+          Task.builder(() -> message.edit(message.component())).build().executeOnRenderThread();
+        } catch (Exception e) {
+          Logging.getLogger().error("Failed to translate message: " + e);
+        }
+      }
+    }
+  }
+
   @Subscribe
   public void onChatReceive(ChatReceiveEvent event) {
     // TODO: Do not translate player names
@@ -32,22 +59,16 @@ public class ChatReceiveListener {
     List<Component> messageChildren = message.component().getChildren();
 
     Task.builder(() -> {
-        for (Component child : messageChildren) {
-            if (child instanceof TextComponent textComponent) {
-                Logging.getLogger().info(textComponent.getText());
+      if (messageChildren.isEmpty()){
+        try {
+          String translatedMessage = translator.translate("auto", String.valueOf(targetLanguage), message.getFormattedText());
 
-                if (Objects.equals(textComponent.getText(), "")) continue;
-                if (Objects.equals(textComponent.getText(), " ")) continue;
-
-                try {
-                    textComponent.text(translator.translate("auto", String.valueOf(targetLanguage), textComponent.getText()));
-
-                    Task.builder(() -> message.edit(message.component())).build().executeOnRenderThread();
-                } catch (Exception e) {
-                    Logging.getLogger().error("Failed to translate message: " + e);
-                }
-            }
+          Task.builder(() -> message.edit(Component.text(translatedMessage))).build().executeOnRenderThread();
+        } catch (Exception e) {
+          Logging.getLogger().error("Failed to translate message: " + e);
         }
+      }else{translateMessage(messageChildren, message);}
+
     }).build().execute();
   }
 }
